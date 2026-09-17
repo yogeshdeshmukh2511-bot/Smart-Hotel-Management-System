c359e7271a26e982e8df78583e43215a9f69840b
Yogesh Deshmukh <yogeshdeshmukh2511@gmail.com>
2026-09-18 00:26:42 +0530

    Fix STATIC_ROOT syntax

--- hotelmanagement/hotelmanagement/settings.py
+++ hotelmanagement/hotelmanagement/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"s
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Email
